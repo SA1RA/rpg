5465d3a0d00cf193235a7423ea9f1e3d5bbf3c9d
Samuel Lucas <smlucas13@gmail.com>
2016-09-17 12:05:20 -0400

    Update rpg.js

--- main/rpg_game/rpg.js
+++ main/rpg_game/rpg.js
@@ -80,6 +80,8 @@ class PlayState {
     
     // player
     this.player = game.add.sprite(C.player.startx,C.player.starty,'player');
+    game.physics.p2.enable(this.player);
+    this.player.enableBody
     console.log(this.player);
     //this.player.smoothed = false; 
     //this.player.scale.set(3);
